ab44c8951a35ff177226693b73a14a2d98772a1d
Anilsoni27 <aksoni8470@gmail.com>
2026-08-07 18:25:49 +0530

    if_else

---
+++ if_else.java
@@ -0,0 +1,21 @@
+public class if_else {
+    public static void main(String [] args){
+        int x = 5;
+        int y = 7;
+
+        if(x > y){
+            System.out.println(x);
+
+        }
+        else
+            System.out.println(y);
+
+        // if(x > 10 && x <= 20){
+        //     System.out.println("Hello");
+        // }
+        // else
+        //  System.out.println("Bye");
+
+    }
+    
+}
