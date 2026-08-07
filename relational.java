1a93d7d396fee499bad537c1008f9df9679472b2
Anilsoni27 <aksoni8470@gmail.com>
2026-08-07 17:57:32 +0530

    Relational operator

---
+++ relational.java
@@ -0,0 +1,26 @@
+public class relational {
+    public static void main(String [] args){
+        int a=2;
+        int b=23;
+        byte c=2;
+
+        double k = 23.5;
+        int d = (int)k;
+
+        boolean res = b == d;
+
+        System.out.println(res);
+
+
+
+        // boolean res1 = a <= b;
+        // boolean res2 = a != b;
+        // boolean res3 = a == c;
+
+
+        // System.out.println(res1);
+        // System.out.println(res2);
+        // System.out.println(res3);
+    }
+    
+}
