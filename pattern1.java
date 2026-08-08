d2b4c87b799cc526b493d069737a47f45a6f16f7
Anilsoni27 <aksoni8470@gmail.com>
2026-08-08 11:20:13 +0530

    Suare pattern

---
+++ pattern1.java
@@ -0,0 +1,20 @@
+// * * * * * * * 
+//      *
+//      * 
+//      * 
+//      * 
+//      * 
+//      * 
+public class pattern1 {
+    public static void main(String []  args){
+        for (int i = 1; i <= 7 ; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        for(int i = 1; i <= 6; i++){ 
+             System.out.println("      *");
+        }
+        
+    }
+    
+}
