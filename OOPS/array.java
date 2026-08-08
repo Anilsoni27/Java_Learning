e3cc93574afaf4d5fcfa0a08850990b7cc6869cd
Anilsoni27 <aksoni8470@gmail.com>
2026-08-08 22:20:06 +0530

    Array

---
+++ OOPS/array.java
@@ -0,0 +1,21 @@
+public class array {
+    public static void main(String[] args) {
+        // int num [] = {1,2,3,4,5};
+
+        // System.out.println(num[0]);
+        // num[2] = 6;
+        // System.out.println(num[2]);
+
+        int nums [] = new int [4];
+
+        nums[0] = 4 ;
+        nums[1] = 5 ;
+        nums[2] = 6 ;
+        nums[3] = 7 ;
+        System.out.println(nums[3]);
+
+        for(int i = 0 ; i < 4 ; i++)
+            System.out.print(nums[i]+" ");
+
+    }
+}
