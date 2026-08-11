3393796ebcbe12a5ef9680d23bcd0989a03c8edf
Anilsoni27 <aksoni8470@gmail.com>
2026-08-11 11:23:21 +0530

    Overriding

---
+++ OOPS/MethodOverriding.java
@@ -0,0 +1,27 @@
+// ===== Method Overriding =====
+class Calc {
+
+    public int add (int a , int b){
+        return a + b;
+    }
+    public int sub(int a , int b){
+        return a-b;
+    }
+
+}
+class AdvaCalc extends Calc {
+    public int add(int a , int b){
+        return a + b + 1;
+    }
+}
+public class MethodOverriding {
+    public static void main(String [] args){
+
+        AdvaCalc obj = new AdvaCalc();
+        int r1 = obj.add(3, 4);
+        int r2 = obj.sub(8,4);
+        System.out.println(r1);
+        System.out.println(r2);
+    }
+    
+}
