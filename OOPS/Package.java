e1e7887e18d14f9b7ae455da1720dd96c8dba0d6
Anilsoni27 <aksoni8470@gmail.com>
2026-08-11 12:11:31 +0530

    Upadated

--- OOPS/Package.java
+++ OOPS/Package.java
@@ -1,12 +1,13 @@
-import tools.Calc;
-import tools.AdvCalc;
+import other.tools.AdvCalc;
+import other.tools.Calc;
 
-// import tools.*; // * means all file not the folder
+// import other.*; // * means all file not the folder
 
 public class Package {
     public static void main (String [] args){
         Calc obj = new Calc();
         AdvCalc obj2 = new AdvCalc();
+        A obj3 = new A();
 
         System.out.println(obj.add(2,3));
         System.out.println(obj2.mul(2,3));
