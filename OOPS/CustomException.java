27463a1e2d644f8c361060b6baaaa9d7ac5c92eb
Anilsoni27 <aksoni8470@gmail.com>
2026-08-12 17:21:20 +0530

    Custom exception

---
+++ OOPS/CustomException.java
@@ -0,0 +1,28 @@
+// For custom exception make a class which extends Exception class and if we wnat to give any parameter in constructor so we have to call super
+class MyException extends Exception{
+    public MyException(String str){
+        
+        super(str);
+    }
+}
+public class CustomException {
+    public static void main (String [] args){
+
+        int i = 20;
+        int j = 0;
+
+        try{
+            j = 18 / i;
+            if(j == 0){
+                throw new MyException ("I dont want exception" );
+            }
+        }
+        catch(MyException e){
+            j = 18 / 1;
+            
+            System.out.println("Thats default output: "+ e );
+        }
+        System.out.println(j);
+    }
+    
+}
---
+++ OOPS/ThrowKeyword.java
@@ -0,0 +1,23 @@
+public class ThrowKeyword {
+    
+    public static void main(String[] args) {
+        
+        int i = 20;
+        int j = 0;
+
+        try{
+            j = 18 / i;
+            if(j == 0){
+                throw new ArithmeticException ("I dont want exception" );
+            }
+        }
+        catch(ArithmeticException e){
+            j = 18 / 1;
+            
+            System.out.println("Thats default output: "+ e );
+        }
+        System.out.println(j);
+        
+    }
+}  
+
