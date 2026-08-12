4bf371ecfd0eea3d43857ab3f4e00ffd333c42f3
Anilsoni27 <aksoni8470@gmail.com>
2026-08-13 01:16:53 +0530

    Runnable thread

--- OOPS/tempCodeRunnerFile.java
+++ OOPS/tempCodeRunnerFile.java
@@ -1,2 +1,29 @@
-System.out.println(arr[2]);
-            // Sy
+lass A implements  Runnable {
+//     public void run(){
+//         for (int i = 0; i < 10; i++){
+
+//             System.out.println("Hii");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+
+//         }
+//     }
+// }
+// class B implements Runnable {
+//     public void run (){
+        
+//         for (int i = 0; i < 10; i++) {
+
+//             System.out.println("Hellooo");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+            
+//         }
+//     }
+// }
